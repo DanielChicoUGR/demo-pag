7d14375f5bce1cdbbe929fffb6be9e96bff40c8d
daniel <dachival@correo.ugr.es>
2024-04-06 17:58:41 +0200

    astro config actualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,42 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
-const SITE_URL = "https://danielchicougr.github.io";
-// const SITE_URL = "http://localhost:4321";
+/* 
+  We are doing some URL mumbo jumbo here to tell Astro what the URL of your website will be.
+  In local development, your SEO meta tags will have localhost URL.
+  In built production websites, your SEO meta tags should have your website URL.
+  So we give our website URL here and the template will know what URL to use 
+  for meta tags during build.
+  If you don't know your website URL yet, don't worry about this
+  and leave it empty or use localhost URL. It won't break anything.
+*/
 
-// https://astro.build/config
+const SERVER_PORT = 4321;
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const LIVE_URL = "https://danielchicougr.github.io";
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+let base = null;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+  base = "demo_pag";
+}
+
 export default defineConfig({
-  site: SITE_URL,
-  base: "demo-pag",
-  output: "static",
-
-  integrations: [mdx(), sitemap()],
+  server: { port: SERVER_PORT },
+  site: BASE_URL,
+  base: base,
+  integrations: [
+    sitemap(),
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+  ],
 });
