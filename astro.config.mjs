af0731845cd8c40ab6dd22fa6172cd12c6134231
daniel <dachival@correo.ugr.es>
2024-04-06 17:41:41 +0200

    links actualizados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 const SITE_URL = "https://danielchicougr.github.io";
 // const SITE_URL = "http://localhost:4321";
 
 // https://astro.build/config
 export default defineConfig({
-	site: SITE_URL,
-	base: "demo-pag",
-	integrations: [mdx(), sitemap()],
+  site: SITE_URL,
+  base: "demo-pag",
+  output: "static",
+
+  integrations: [mdx(), sitemap()],
 });
