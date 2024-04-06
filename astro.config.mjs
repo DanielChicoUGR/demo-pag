9fc9212aa270eeffe155a3f678a1acc44f0ba67c
daniel <dachival@correo.ugr.es>
2024-04-06 20:25:32 +0200

    astro config actualizado y funcionando en local, no carga de imagenes en remoto

--- astro.config.mjs
+++ astro.config.mjs
@@ -50,15 +50,19 @@ const LIVE_URL = "https://danielchicougr.github.io";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
+let base = "";
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
   BASE_URL = LIVE_URL;
+  base = "/demo_pag";
 }
 // https://astro.build/config
 export default defineConfig({
   site: BASE_URL,
   output: "static",
-  // base:base,
+  base:base,
+  trailingSlash:"always",
+  
 
   integrations: [mdx(), sitemap()],
 });
