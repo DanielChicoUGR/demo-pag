ef8214da8b99c8eae2fdf91f50b661996ec34de6
daniel <dachival@correo.ugr.es>
2024-04-06 19:30:39 +0200

    astro config actualizado y funcionando remoto?

--- astro.config.mjs
+++ astro.config.mjs
@@ -50,17 +50,15 @@ const LIVE_URL = "https://danielchicougr.github.io";
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
-let base = "";
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
   BASE_URL = LIVE_URL;
-  base = "/demo_pag";
 }
 // https://astro.build/config
 export default defineConfig({
   site: BASE_URL,
-  // base: base,
-  // output: "static",
+  output: "static",
+  // base:base,
 
   integrations: [mdx(), sitemap()],
 });
