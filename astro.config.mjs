55c623af5eb65b72fc55b4bc5ebfbc5e5ba78168
daniel <dachival@correo.ugr.es>
2024-04-09 13:44:10 +0200

    astro config actualizado y funcionando en local, no carga de imagenes en remoto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,23 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-const SERVER_PORT = 4321;
-// the url to access your blog during local development
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-// the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = 'https://danielchicougr.github.io';
-// this is the astro command your npm script runs
-const SCRIPT = process.env.npm_lifecycle_script || '';
-const isBuild = SCRIPT.includes('astro build');
+import {
+  LOCALHOST_URL, LIVE_URL, IS_BUILD, REPO_NAME,
+} from './src/consts.ts';
+
 let BASE_URL = LOCALHOST_URL;
-let base = '';
+let base = REPO_NAME;
+
 // When you're building your site in local or in CI, you could just set your URL manually
-if (isBuild) {
+if (IS_BUILD) {
   BASE_URL = LIVE_URL;
-  base = '/demo_pag';
+} else {
+  base = '/';
 }
 export default defineConfig({
   site: BASE_URL,
   output: 'static',
-  base: base,
+  base,
   trailingSlash: 'always',
   integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,13 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const SERVER_PORT = 4321;
+// the url to access your blog during local development
+export const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+export const LIVE_URL = 'https://danielchicougr.github.io';
+// this is the astro command your npm script runs
+export const SCRIPT = process.env.npm_lifecycle_script || '';
+export const IS_BUILD = SCRIPT.includes('astro build');
+export const REPO_NAME = '/demo_page';
