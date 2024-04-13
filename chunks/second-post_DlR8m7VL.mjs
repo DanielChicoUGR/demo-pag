e65c9f92350c0e492a0d4c94ec9bef60cdb99c86
daniel <dachival@correo.ugr.es>
2024-04-13 17:19:59 +0200

    Updates

--- chunks/second-post_DlR8m7VL.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./second-post_BcuVgDgL.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
